affb084d699fa18116321cc519fb111d6feff108
Aditya Kumar Singh <sadityakumar9211@gmail.com>
2022-08-21 04:59:13 +0530

    initial commit

---
+++ makelex.sh
@@ -0,0 +1,31 @@
+# Author: Aditya Kumar Singh (2020BCS-004)
+# License: MIT
+
+#!/bin/zsh
+
+GREEN='\033[1;32m'
+DARKGREEN='\033[1;32m'
+PURPLE='\033[0;35m'
+NC='\033[0m'  #No Color
+echo "${GREEN}Generating Lexical Analyser...${NC}" &&
+echo "${GREEN}Script Name:${NC} $0" &&
+echo "${GREEN}Lexical File Name:${NC} $1\n" &&
+
+mkdir generated_files
+
+flex -o generated_files/lex.yy.c $1 &&
+cc generated_files/lex.yy.c -S -o generated_files/lex.yy.s &&
+
+
+FILENAME=$1 &&
+
+OUTPUT_FILE="${FILENAME%.l}_output" &&
+cc generated_files/lex.yy.c -o generated_files/${OUTPUT_FILE}&&
+
+echo "${DARKGREEN}Lexical Analyser Generated Successfully!${NC}" &&
+
+echo "${PURPLE}Running output file...${NC}" &&
+echo "" &&
+./generated_files/$OUTPUT_FILE
+
+
